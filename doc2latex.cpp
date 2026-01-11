ed32a612fc0329fe7c769d70fd10efee2f91368d
un1u3 <python.github.devs@gmail.com>
2026-01-11 20:38:30 +0545

    init

---
+++ doc2latex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <zip.h> //for reading ZIP arc
+#include <tinyxml2.h> // for XML parsing
+
+int main(){
+
+
+
+}
